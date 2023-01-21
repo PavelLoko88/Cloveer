e1aaf4891e579cfc79f8ffa1037a7050bbfe766c
PavelLoko88 <118975859+PavelLoko88@users.noreply.github.com>
2023-01-21 23:02:41 +0400

    Add files via upload

--- script/script.js
+++ script/script.js
@@ -1,27 +1,43 @@
 let toglle = false;
 const content = document.querySelector('body');
-
 const hamb = document.getElementById('hamberger');
+const hambClose = document.getElementById('hambClose');
+const hambOpen = document.getElementById('hambOpen');
 const hideList = document.getElementById('hide-list');
-
 const ghost = document.getElementById('ghost');
 // BLUR
 const img = document.getElementById('img');
-const blur = document.getElementById('blurs')
-
-
+const blur = document.getElementById('blurs');
+let i = 0;
+hambClose.style.display = 'none';
+hambOpen.style.display = 'block';
 hamb.addEventListener("click", () => {
     toglle != toglle;
     if (toglle) {
         hideList.classList.toggle('open');
         ghost.classList.toggle('ghost-open');
         blur.classList.toggle('offblur');
-
     } else {
         hideList.classList.toggle('close');
         ghost.classList.toggle('ghost-close');
         img.classList.toggle('margin-up');
         blur.classList.toggle('blur');
         content.classList.toggle('fixed');
+
+
     }
+
+    i++;
+    console.log(i);
+    if (i == 1) {
+
+        hambClose.style.display = 'block';
+        hambOpen.style.display = 'none';
+    } else if (i > 1) {
+        i = 0;
+        hambClose.style.display = 'none';
+        hambOpen.style.display = 'block';
+    }
+
+
 })
